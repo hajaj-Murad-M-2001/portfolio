cacaacc8b53f006dbf5427679f876b47172dd88e
Murad Hajaj <hajajmurad@gmail.com>
2026-09-25 10:29:12 +0300

    Fix experience view case-sensitivity and routes configuration

--- app/Http/Controllers/ExperienceController.php
+++ app/Http/Controllers/ExperienceController.php
@@ -10,6 +10,6 @@ class ExperienceController extends Controller
     {
         $experiences = Experience::ordered()->get();
 
-        return view('experience', compact('experiences'));
+        return view('Experience', compact('experiences'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ExperienceController;
 use App\Models\Project;
 
 // عرض الصفحة الرئيسية للبورتفوليو مع جلب المشاريع والمهارات
@@ -10,15 +12,14 @@ Route::get('/', [HomeController::class, 'index']);
 // استقبال وحفظ رسائل نموذج الاتصال
 Route::post('/contact', [HomeController::class, 'storeMessage'])->name('contact.store');
 
-Route::get('/about', function () {
-    return view('about');
-})->name('about');
+// عرض صفحة About عبر AboutController لتمرير متغير $stats
+Route::get('/about', [AboutController::class, 'index'])->name('about');
 
+// عرض صفحة المشاريع
 Route::get('/projects', function () {
-    // يمكنك جلب المشاريع من قاعدة البيانات أو تمريرها كمصفوفة
     $projects = Project::all(); 
     return view('projects', compact('projects'));
 })->name('projects');
 
-// الطريقة الصحيحة لعرض صفحة تعتمد على View مباشرة باستخدام Route::view
-Route::view('/experience', 'Experience')->name('experience');
+// عرض صفحة Experience عبر ExperienceController
+Route::get('/experience', [ExperienceController::class, 'index'])->name('experience');
